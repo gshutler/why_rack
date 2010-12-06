a35a108929e90d21f3e6227d18788fcc711a36fe
Garry Shutler <garry@robustsoftware.co.uk>
2010-12-06 20:32:55 +0000

    added url to the sintra intro

--- server.rb
+++ server.rb
@@ -1,6 +1,11 @@
 require 'rubygems'
 require 'sinatra/base'
 
+# Define a class whose instances can be mounted as Sinatra applications.
+#
+# See http://www.sinatrarb.com/intro.html for a introduction to Sinatra which 
+# should help you understand this example clear if you aren't familiar with
+# Sinatra.
 class Server < Sinatra::Base
 
   get '/hello/:person' do |person|
