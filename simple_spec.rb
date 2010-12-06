91f89b9f4f632c9599214620fefcc58945f45f9f
Garry Shutler <garry@robustsoftware.co.uk>
2010-12-06 07:20:29 +0000

    additional comments to explain for non-rubyists

--- simple_spec.rb
+++ simple_spec.rb
@@ -4,18 +4,21 @@ require 'rack/test' # the rack test helpers
 require 'rspec'     # our testing framework
 
 describe Server, 'making a hello request' do
-
+  # make the rack test helpers available  
   include Rack::Test::Methods
-
+  
+  # tell rack what code to use to handle the request
   let(:app) {subject}
   
   describe 'saying hello to world' do
   
     before do
+      # invoke a rack request with for the given URL
       get '/hello/world'
     end
     
     it 'should respond "Hello, world"' do
+      # check that the body of the response to the request is as expected
       last_response.body.should == 'Hello, world'
     end
   
@@ -24,10 +27,12 @@ describe Server, 'making a hello request' do
   describe 'saying hello to Garry' do
   
     before do
+      # invoke a rack request with for the given URL
       get '/hello/Garry'
     end
     
     it 'should respond "Hello, Garry"' do
+      # check that the body of the response to the request is as expected
       last_response.body.should == 'Hello, Garry'
     end
   
